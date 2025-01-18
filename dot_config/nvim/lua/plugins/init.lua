f16734af569438bcf4df768d2cbc896bf5723063
Tony <tonybanh@live.com>
2025-01-17 22:05:55 -0800

    add mini-operator

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -87,6 +87,30 @@ return {
     end,
   },
 
+  {
+    "echasnovski/mini.operators",
+    version = "*",
+    opts = {
+      -- Exchange text regions
+      exchange = {
+        prefix = "gx",
+        -- Whether to reindent new text to match previous indent
+        reindent_linewise = true,
+      },
+      -- multiple (duplicate) text
+      multiply = {
+        prefix = "gm",
+      },
+      -- Evaluate text and replace with output
+      evaluate = {}, -- evaluate text and replace with output
+      replace = {}, -- replace text with register
+      sort = {}, -- sort text
+    },
+    config = function(_, opts)
+      require("mini.operators").setup(opts)
+    end,
+  },
+
   -- https://github.com/epwalsh/obsidian.nvim
   {
     "epwalsh/obsidian.nvim",
