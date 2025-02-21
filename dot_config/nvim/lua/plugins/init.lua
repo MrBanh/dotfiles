2f6de2967f840f8e416f20d54a0203121b32251f
Tony Banh <tbanh@hubspot.com>
2025-02-20 19:58:10 -0800

    Remove yazi nvim, use snacks explorer floating preset

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -433,7 +433,8 @@ return {
         },
         sources = {
           explorer = {
-            layout = { layout = { position = "right" } },
+            auto_close = true,
+            layout = { preset = "default", preview = true },
           },
         },
         win = {
@@ -454,7 +455,6 @@ return {
       terminal = {
         win = {
           style = {
-            position = "float",
             border = "rounded",
           },
         },
@@ -583,45 +583,5 @@ return {
     opts = {
       preset = "classic",
     },
-  },
-
-  {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>yf",
-        "<cmd>Yazi<cr>",
-        desc = "Open yazi at the current file",
-      },
-      {
-        -- Open in the current working directory
-        "<leader>yw",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
-      },
-      {
-        -- NOTE: this requires a version of yazi that includes
-        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-        "<leader>ys",
-        "<cmd>Yazi toggle<cr>",
-        desc = "Resume the last yazi session",
-      },
-    },
-    ---@type YaziConfig
-    opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
-      keymaps = {
-        show_help = "<f1>",
-      },
-    },
-    config = function(_, opts)
-      require("yazi").setup(opts)
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>y", group = "Yazi", icon = { icon = "󰇥 ", color = "yellow", cat = "extension" } },
-      })
-    end,
   },
 }
