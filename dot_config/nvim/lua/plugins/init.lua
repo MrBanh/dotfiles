70df1bd59953646ce3c66e2964629f86c7c2a1c7
Tony <tonybanh@live.com>
2025-02-23 05:43:10 -0800

    change keybinding for snacks explorer and yazi nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -447,6 +447,14 @@ return {
           explorer = {
             auto_close = true,
             layout = { preset = "default", preview = true },
+            win = {
+              list = {
+                keys = {
+                  ["<c-/>"] = "terminal",
+                  ["<c-\\>"] = "tcd",
+                },
+              },
+            },
           },
         },
         win = {
@@ -623,9 +631,18 @@ return {
     ---@type YaziConfig
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
+      open_for_directories = true,
       keymaps = {
-        show_help = "<f1>",
+        show_help = "g?",
+        open_file_in_horizontal_split = "<c-s>",
+        open_file_in_vertical_split = "<c-v>",
+        open_file_in_tab = "<c-t>",
+        grep_in_directory = nil,
+        replace_in_directory = "<c-g>",
+        cycle_open_buffers = "<tab>",
+        copy_relative_path_to_selected_files = "<c-y>",
+        send_to_quickfix_list = "<c-q>",
+        change_working_directory = "<c-\\>",
       },
     },
     config = function(_, opts)
