fdcdb84c87a8c0bc0f3ce2b417f4bdbf607c571a
Tony <tonybanh@live.com>
2025-02-23 00:05:57 -0800

    remove mini hipattern, it's not needed

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -148,19 +148,6 @@ return {
   },
 
   {
-    "echasnovski/mini.hipatterns",
-    opts = {
-      highlighters = {
-        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-      },
-    },
-  },
-
-  {
     "skardyy/neo-img",
     build = function()
       require("neo-img").install()
