493718b548da2aa3d31f9fd7649ab1fa68de9cf2
Tony Banh <tbanh@hubspot.com>
2025-03-19 11:02:28 -0700

    Remove Avante

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,142 +1,4 @@
 return {
-  -- {
-  --   "yetone/avante.nvim",
-  --   event = "VeryLazy",
-  --   lazy = false,
-  --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-  --   opts = {
-  --     debug = true,
-  --     provider = "copilot",
-  --
-  --     copilot = {
-  --       endpoint = "https://api.githubcopilot.com",
-  --       model = "o3-mini", -- https://docs.github.com/en/copilot/using-github-copilot/ai-models/changing-the-ai-model-for-copilot-chat
-  --       proxy = nil, -- [protocol://]host[:port] Use this proxy
-  --       allow_insecure = false, -- Allow insecure server connections
-  --       timeout = 30000, -- Timeout in milliseconds
-  --       temperature = 0,
-  --       max_tokens = 4096,
-  --       disable_tools = true,
-  --     },
-  --
-  --     openai = {
-  --       endpoint = "https://openrouter.ai/api/v1",
-  --       -- model = "google/gemini-2.0-flash-exp:free",
-  --       model = "meta-llama/llama-3.1-70b-instruct:free",
-  --       timeout = 30000, -- Timeout in milliseconds
-  --       temperature = 0,
-  --       max_tokens = 40000,
-  --     },
-  --
-  --     file_selector = {
-  --       provider = "snacks", -- native|fzf|mini.pick|snacks|telescope
-  --       provider_opts = {},
-  --     },
-  --
-  --     -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
-  --     system_prompt = function()
-  --       local hub = require("mcphub").get_hub_instance()
-  --       return hub:get_active_servers_prompt()
-  --     end,
-  --     -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
-  --     custom_tools = function()
-  --       return {
-  --         require("mcphub.extensions.avante").mcp_tool(),
-  --       }
-  --     end,
-  --   },
-  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  --   build = vim.fn.has("win32") == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-  --     or "make",
-  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  --   dependencies = {
-  --     "stevearc/dressing.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     "ravitemer/mcphub.nvim",
-  --
-  --     --- The below dependencies are optional
-  --
-  --     -- pickers
-  --     --- "nvim-telescope/telescope.nvim",
-  --     -- "ibhagwan/fzf-lua",
-  --
-  --     -- autocompletion
-  --     --- "hrsh7th/nvim-cmp",
-  --     "saghen/blink.cmp",
-  --
-  --     -- icons
-  --     "echasnovski/mini.icons",
-  --     -- "nvim-tree/nvim-web-devicons",
-  --
-  --     -- providers
-  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --
-  --     -- misc
-  --     {
-  --       -- support for image pasting
-  --       "HakonHarnes/img-clip.nvim",
-  --       event = "VeryLazy",
-  --       opts = {
-  --         -- recommended settings
-  --         default = {
-  --           embed_image_as_base64 = false,
-  --           prompt_for_file_name = false,
-  --           drag_and_drop = {
-  --             insert_mode = true,
-  --           },
-  --           -- required for Windows users
-  --           use_absolute_path = true,
-  --         },
-  --       },
-  --     },
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = {
-  --         file_types = { "markdown", "Avante" },
-  --       },
-  --       ft = { "markdown", "Avante" },
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     require("avante").setup(opts)
-  --     require("which-key").add({
-  --       { "<leader>a", group = "ai", icon = { icon = "󰚩 ", color = "green", cat = "extension" } },
-  --     })
-  --   end,
-  -- },
-  --
-  -- {
-  --   "saghen/blink.cmp",
-  --   dependencies = {
-  --     "Kaiser-Yang/blink-cmp-avante",
-  --     "rafamadriz/friendly-snippets",
-  --     {
-  --       "saghen/blink.compat",
-  --       optional = true, -- make optional so it's only enabled if any extras need it
-  --       opts = {},
-  --       version = not vim.g.lazyvim_blink_main and "*",
-  --     },
-  --   },
-  --   opts = {
-  --     sources = {
-  --       -- Add 'avante' to the list
-  --       default = { "avante", "lsp", "path", "snippets", "buffer" },
-  --       providers = {
-  --         avante = {
-  --           module = "blink-cmp-avante",
-  --           name = "Avante",
-  --           opts = {
-  --             -- options for blink-cmp-avante
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
-  --
-
   {
     "olimorris/codecompanion.nvim",
     dependencies = {
@@ -157,6 +19,42 @@ return {
             schema = {
               model = {
                 default = "deepseek/deepseek-r1:free",
+              },
+            },
+          })
+        end,
+        claude = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "claude-3.7-sonnet",
+              },
+              max_tokens = {
+                default = 65536,
+              },
+            },
+          })
+        end,
+        o3 = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "o3-mini-2025-01-31",
+              },
+              max_tokens = {
+                default = 65536,
+              },
+            },
+          })
+        end,
+        ["4o"] = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "gpt-4o",
+              },
+              max_tokens = {
+                default = 65536,
               },
             },
           })
@@ -187,8 +85,6 @@ return {
     },
 
     keys = {
-      { mode = "n", "<leader>ac", "<CMD>CodeCompanionChat Toggle<CR>", silent = true, desc = "CodeCompanion chat" },
-      { mode = "v", "<leader>aa", "<CMD>CodeCompanionActions<CR>", silent = true, desc = "CodeCompanion actions" },
       {
         mode = "n",
         "<leader>a:",
@@ -198,7 +94,41 @@ return {
         end,
         desc = "CodeCompanionCmd",
       },
-      { mode = "v", "<leader>ay", "<CMD>CodeCompanionChat Add<CR>", silent = true, desc = "CodeCompanion add" },
+      {
+        mode = { "n", "v" },
+        "<leader>aa",
+        "<CMD>CodeCompanionActions<CR>",
+        silent = true,
+        desc = "CodeCompanion actions",
+      },
+      {
+        mode = { "n" },
+        "<leader>ab",
+        "<CMD>CodeCompanionChat claude<CR>@editor #buffer<CR>",
+        silent = true,
+        desc = "CodeCompanion add buffer to chat",
+      },
+      {
+        mode = { "v" },
+        "<leader>ab",
+        "<CMD>CodeCompanionChat claude<CR><CR>@editor #buffer<CR>",
+        silent = true,
+        desc = "CodeCompanion add buffer to chat",
+      },
+      {
+        mode = { "n", "v" },
+        "<leader>ac",
+        "<CMD>CodeCompanionChat Toggle<CR>",
+        silent = true,
+        desc = "CodeCompanion chat",
+      },
+      {
+        mode = "v",
+        "<leader>ay",
+        "<CMD>CodeCompanionChat Add<CR>",
+        silent = true,
+        desc = "CodeCompanion add selection to chat",
+      },
       {
         mode = "n",
         "<leader>ag",
