75aa12b7525dc32a3630fb01418937458945afb6
Tony Banh <tbanh@hubspot.com>
2025-03-25 03:48:40 -0700

    Add scratch.nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -732,4 +732,18 @@ return {
       })
     end,
   },
+
+  {
+    "LintaoAmons/scratch.nvim",
+    cmd = { "Scratch", "ScratchWithName", "ScratchOpen", "ScratchOpenFzf" },
+    keys = {
+      { "<leader>Sn", "<cmd>Scratch<cr>", desc = "new scratch" },
+      { "<leader>SN", "<cmd>ScratchWithName<cr>", desc = "new scratch (named)" },
+      { "<leader>So", "<cmd>ScratchOpen<cr>", desc = "open scratch" },
+      { "<leader>Sg", "<cmd>ScratchOpenFzf<cr>", desc = "open scratch (fzf)" },
+    },
+    opts = {
+      filetypes = { "lua", "js", "sh", "ts" },
+    },
+  },
 }
