288f64804383c635ef8248a4fb3579fbfd9ab744
Tony Banh <tbanh@hubspot.com>
2025-04-26 00:25:24 -0700

    remove img-clip

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -97,11 +97,6 @@ return {
   },
 
   {
-    "HakonHarnes/img-clip.nvim",
-    event = "VeryLazy",
-  },
-
-  {
     "kawre/leetcode.nvim",
     build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
     dependencies = {
