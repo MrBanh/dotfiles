8992ee8b98c7aec7f49dacb91b8bba627685b2bb
Tony Banh <tbanh@hubspot.com>
2025-05-09 00:16:46 -0700

    Pin Mason in its own module

---
+++ dot_config/nvim/lua/plugins/mason/mason.lua
@@ -0,0 +1,5 @@
+return {
+  -- Breaking change released in 2.0, use pinned v1 for now
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
