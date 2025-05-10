a4c66c313483d80402562befc97562e24696d795
Tony Banh <tbanh@hubspot.com>
2025-05-10 03:35:15 -0700

    Add border for docs/signature help via Noice

--- dot_config/nvim/lua/plugins/editor/noice.lua
+++ dot_config/nvim/lua/plugins/editor/noice.lua
@@ -3,6 +3,7 @@ return {
   opts = {
     presets = {
       bottom_search = false, -- use a classic bottom cmdline for search
+      lsp_doc_border = true, -- add a border to hover docs and signature help
     },
   },
 }
