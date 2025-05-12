3b6724c02e77d6a44ffc53d1455cf692ab2aa5ac
Tony Banh <tbanh@hubspot.com>
2025-05-12 00:03:42 -0700

    Create autocmd for searching in browser from nvim

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -83,3 +83,44 @@ end, {
   desc = "Get a sourcegraph link to the current location",
   range = 2,
 })
+
+vim.api.nvim_create_user_command("SearchInBrowser", function(args)
+  local config = {
+    default_engine = "google",
+    query_map = {
+      google = "https://www.google.com/search?q=%s",
+    },
+  }
+
+  local function looks_like_url(input)
+    local pat = "[%w%.%-_]+%.[%w%.%-_/]+"
+    return input:match(pat) ~= nil
+  end
+
+  local function extract_prefix(input)
+    local pat = "@(%w+)"
+    local prefix = input:match(pat)
+    if not prefix or not config.query_map[prefix] then
+      return vim.trim(input), config.default_engine
+    end
+    local query = input:gsub("@" .. prefix, "")
+    return vim.trim(query), prefix
+  end
+
+  local function query_browser(input)
+    local q, prefix = extract_prefix(input)
+    if not looks_like_url(input) then
+      local format = config.query_map[prefix]
+      q = format:format(vim.uri_encode(q))
+    end
+    vim.ui.open(q)
+  end
+
+  vim.ui.input({ prompt = "Search: " }, function(input)
+    if input then
+      query_browser(input)
+    end
+  end)
+end, {
+  desc = "Search in browser",
+})
