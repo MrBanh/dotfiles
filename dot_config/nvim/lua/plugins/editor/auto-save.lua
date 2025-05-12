dd1678bcb5efd0db26f8c42133785bbfd7bfc0b6
Tony Banh <tbanh@hubspot.com>
2025-05-12 00:03:29 -0700

    auto-save only on insert leave

--- dot_config/nvim/lua/plugins/editor/auto-save.lua
+++ dot_config/nvim/lua/plugins/editor/auto-save.lua
@@ -2,7 +2,7 @@ return {
   "okuuva/auto-save.nvim",
   version = "*",
   cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  event = { "InsertLeave" }, -- optional for lazy loading on trigger events
   opts = {
     condition = function(buf)
       local filetype = vim.fn.getbufvar(buf, "&filetype")
