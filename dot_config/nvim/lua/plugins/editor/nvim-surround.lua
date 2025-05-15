5d0c81d1cca4e8691963a8e37f183fbd07e74df7
Tony Banh <tbanh@hubspot.com>
2025-05-15 03:07:04 -0700

    Update nvim surround key mapping

--- dot_config/nvim/lua/plugins/editor/nvim-surround.lua
+++ dot_config/nvim/lua/plugins/editor/nvim-surround.lua
@@ -3,7 +3,12 @@ return {
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   opts = {
+    -- defaults: https://github.com/kylechui/nvim-surround/blob/main/lua/nvim-surround/config.lua
     keymaps = {
+      normal = "gs",
+      normal_cur = "gss",
+      normal_line = "gS",
+      normal_cur_line = "gSS",
       visual = "gs",
     },
   },
