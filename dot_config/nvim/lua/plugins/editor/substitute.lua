b185dcdea04ef824ff3ec6818170c3f7666d5035
Tony Banh <tbanh@hubspot.com>
2025-05-15 03:06:58 -0700

    Delete mini.operators, update substitute nvim for exchange keymapping

--- dot_config/nvim/lua/plugins/editor/substitute.lua
+++ dot_config/nvim/lua/plugins/editor/substitute.lua
@@ -1,12 +1,17 @@
 return {
   "gbprod/substitute.nvim",
-  lazy = false,
-  opts = {},
-  config = function(_, opts)
-    require("substitute").setup(opts)
-    vim.keymap.set("n", "r", require("substitute").operator, { noremap = true })
-    vim.keymap.set("x", "r", require("substitute").visual, { noremap = true })
-    vim.keymap.set("n", "rr", require("substitute").line, { noremap = true })
-    vim.keymap.set("n", "R", require("substitute").eol, { noremap = true })
+  config = function()
+    require("substitute").setup({
+      on_substitute = require("yanky.integration").substitute(),
+    })
+
+    vim.keymap.set("n", "r", require("substitute").operator, { noremap = true, desc = "Substitute" })
+    vim.keymap.set("x", "r", require("substitute").visual, { noremap = true, desc = "Substitute" })
+    vim.keymap.set("n", "rr", require("substitute").line, { noremap = true, desc = "Substitute line" })
+    vim.keymap.set("n", "R", require("substitute").eol, { noremap = true, desc = "Substitute eol" })
+    vim.keymap.set("n", "rx", require("substitute.exchange").operator, { noremap = true, desc = "Exchange" })
+    vim.keymap.set("x", "rx", require("substitute.exchange").visual, { noremap = true, desc = "Exchange" })
+    vim.keymap.set("n", "rxx", require("substitute.exchange").line, { noremap = true, desc = "Exchange line" })
+    vim.keymap.set("n", "rxc", require("substitute.exchange").cancel, { noremap = true, desc = "Cancel exchange" })
   end,
 }
