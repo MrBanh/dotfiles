4edbbdc64846dcd1f6f145f1c7e0a110669f8806
Tony Banh <tbanh@hubspot.com>
2025-05-20 03:04:14 -0700

    Add shadafile for persist global marks per project

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -45,3 +45,12 @@ vim.g.bullets_enable_in_empty_buffers = 0
 
 -- blink, requires NeoVim >= 0.11
 vim.o.winborder = "rounded"
+
+-- shada file
+vim.opt.exrc = true
+vim.opt.secure = true
+local workspace_path = vim.fn.getcwd()
+local cache_dir = vim.fn.stdpath("data")
+local unique_id = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
+local shadafile = cache_dir .. "/myshada/" .. unique_id .. ".shada"
+vim.opt.shadafile = shadafile
