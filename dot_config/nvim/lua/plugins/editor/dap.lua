3ef7d1e2875619c7fc54bfbdae689e822ba504a6
Tony Banh <tbanh@hubspot.com>
2025-05-28 19:24:36 -0700

    Add chrome-debug-adapter

--- dot_config/nvim/lua/plugins/editor/dap.lua
+++ dot_config/nvim/lua/plugins/editor/dap.lua
@@ -1,5 +1,14 @@
 return {
   "mfussenegger/nvim-dap",
+  depedencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "chrome-debug-adapter")
+      end,
+    },
+  },
   keys = {
     {
       "<F5>",
@@ -33,70 +42,24 @@ return {
 
   opts = function()
     local dap = require("dap")
-    local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
 
-    -- Add your custom configurations to existing ones
+    -- adapter definitions
+    if not dap.adapters["chrome"] then
+      dap.adapters["chrome"] = {
+        type = "executable",
+        command = "node",
+        args = {
+          LazyVim.get_pkg_path("chrome-debug-adapter", "out/src/chromeDebug.js"),
+        },
+      }
+    end
+
+    local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
     for _, language in ipairs(js_filetypes) do
-      -- Ensure configurations exist (they should from LazyVim)
       dap.configurations[language] = dap.configurations[language] or {}
 
       -- Add your custom configurations
-      local custom_configs = {
-        {
-          type = "pwa-node",
-          request = "attach",
-          name = "Auto Attach (9229)",
-          port = 9229,
-          restart = true,
-          sourceMaps = true,
-          protocol = "inspector",
-          skipFiles = { "<node_internals>/**" },
-          cwd = "${workspaceFolder}",
-          localRoot = "${workspaceFolder}",
-          continueOnAttach = true,
-        },
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch with args",
-          program = "${file}",
-          args = function()
-            local args_string = vim.fn.input("Arguments: ")
-            return vim.split(args_string, " ")
-          end,
-          cwd = "${workspaceFolder}",
-          console = "integratedTerminal",
-          skipFiles = { "<node_internals>/**" },
-        },
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Debug via npm",
-          runtimeExecutable = "npm",
-          runtimeArgs = function()
-            local script = vim.fn.input("npm script: ")
-            return { "run", script }
-          end,
-          cwd = "${workspaceFolder}",
-          console = "integratedTerminal",
-          skipFiles = { "<node_internals>/**" },
-        },
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Debug Selenium Test",
-          program = "${file}",
-          cwd = "${workspaceFolder}",
-          console = "integratedTerminal",
-          skipFiles = { "<node_internals>/**" },
-          env = {
-            NODE_ENV = "test",
-            SELENIUM_BROWSER = "chrome",
-            HEADLESS = "false",
-          },
-          timeout = 30000,
-        },
-      }
+      local custom_configs = {}
 
       -- Append custom configs to existing ones
       for _, config in ipairs(custom_configs) do
