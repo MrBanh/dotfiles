ceb3d012282702af963dbf4367377ced0ee335f9
Tony Banh <tbanh@hubspot.com>
2025-06-04 21:23:18 -0700

    Lazy: upate template-string

--- dot_config/nvim/lua/plugins/editor/template-string.lua
+++ dot_config/nvim/lua/plugins/editor/template-string.lua
@@ -13,7 +13,7 @@ return {
         "python",
       }, -- filetypes where the plugin is active
       jsx_brackets = true, -- must add brackets to JSX attributes
-      remove_template_string = false, -- remove backticks when there are no template strings
+      remove_template_string = true, -- remove backticks when there are no template strings
       restore_quotes = {
         -- quotes used when "remove_template_string" option is enabled
         normal = [[']],
