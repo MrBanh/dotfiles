79eee6de18d8346bc63f2013a8fa13fd49ceefd5
Tony Banh <tbanh@hubspot.com>
2025-06-06 14:21:36 -0700

    chad: editor plugins
    - auto-save
    - bullets
    - gx
    - nvim-surround
    - nvim-treesitter
    - persistence
    - quicker
    - scratch
    - stay-centered
    - substitute
    - tabout
    - template-string
    - vim-visual-multi

---
+++ dot_config/NvChad/lua/plugins/editor/substitute.lua
@@ -0,0 +1,16 @@
+return {
+  "gbprod/substitute.nvim",
+  config = function()
+    require("substitute").setup({
+      on_substitute = require("yanky.integration").substitute(),
+    })
+
+    vim.keymap.set("n", "r", require("substitute").operator, { noremap = true, desc = "Substitute" })
+    vim.keymap.set("x", "r", require("substitute").visual, { noremap = true, desc = "Substitute" })
+    vim.keymap.set("n", "rr", require("substitute").line, { noremap = true, desc = "Substitute line" })
+    vim.keymap.set("n", "R", require("substitute").eol, { noremap = true, desc = "Substitute eol" })
+    vim.keymap.set("n", "rx", require("substitute.exchange").operator, { noremap = true, desc = "Exchange" })
+    vim.keymap.set("x", "rx", require("substitute.exchange").visual, { noremap = true, desc = "Exchange" })
+    vim.keymap.set("n", "rxx", require("substitute.exchange").line, { noremap = true, desc = "Exchange line" })
+  end,
+}
