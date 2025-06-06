154ac9b19da1a4695d1d25b50522855c8d33fa42
Tony Banh <tbanh@hubspot.com>
2025-06-06 12:24:31 -0700

    chad: change how we group yazi/obsidian

--- dot_config/NvChad/lua/plugins/misc/yazi.lua
+++ dot_config/NvChad/lua/plugins/misc/yazi.lua
@@ -1,52 +1,43 @@
+require("which-key").add { "<leader>y", group = "Yazi", icon = { icon = "󰇥 ", color = "yellow", cat = "extension" } }
+
 return {
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      spec = {
-        { "<leader>y", group = "Yazi", icon = { icon = "󰇥 ", color = "yellow", cat = "extension" } },
-      },
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>yf",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>yF",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      -- NOTE: this requires a version of yazi that includes
+      -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+      "<leader>ys",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
     },
   },
-  {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>yf",
-        "<cmd>Yazi<cr>",
-        desc = "Open yazi at the current file",
-      },
-      {
-        -- Open in the current working directory
-        "<leader>yF",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
-      },
-      {
-        -- NOTE: this requires a version of yazi that includes
-        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-        "<leader>ys",
-        "<cmd>Yazi toggle<cr>",
-        desc = "Resume the last yazi session",
-      },
-    },
-    ---@type YaziConfig
-    opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = true,
-      keymaps = {
-        show_help = "g?",
-        open_file_in_horizontal_split = "<c-s>",
-        open_file_in_vertical_split = "<c-v>",
-        open_file_in_tab = "<c-t>",
-        grep_in_directory = nil,
-        replace_in_directory = "<c-g>",
-        cycle_open_buffers = "]b",
-        copy_relative_path_to_selected_files = "<c-y>",
-        send_to_quickfix_list = "<c-q>",
-        change_working_directory = "`",
-      },
+  ---@type YaziConfig
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = true,
+    keymaps = {
+      show_help = "g?",
+      open_file_in_horizontal_split = "<c-s>",
+      open_file_in_vertical_split = "<c-v>",
+      open_file_in_tab = "<c-t>",
+      grep_in_directory = nil,
+      replace_in_directory = "<c-g>",
+      cycle_open_buffers = "]b",
+      copy_relative_path_to_selected_files = "<c-y>",
+      send_to_quickfix_list = "<c-q>",
+      change_working_directory = "`",
     },
   },
 }
