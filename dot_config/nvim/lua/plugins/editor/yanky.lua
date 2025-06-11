3994feec1f37077b5544dc56d87208d0470d2a62
Tony Banh <tbanh@hubspot.com>
2025-06-11 02:58:03 -0700

    delete yanky keybinding

--- dot_config/nvim/lua/plugins/editor/yanky.lua
+++ dot_config/nvim/lua/plugins/editor/yanky.lua
@@ -1,9 +1,4 @@
 return {
   "gbprod/yanky.nvim",
-  config = function(_, opts)
-    require("yanky").setup(opts)
-
-    vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
-    vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
-  end,
+  opts = {},
 }
