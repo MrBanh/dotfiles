90f2857dcba0b45bbffca78c75b0b080efc41ac2
Tony Banh <tbanh@hubspot.com>
2025-06-13 20:52:59 -0700

    update copilot lsp keymap

--- dot_config/kickstart/lua/plugins/ai/copilot.lua
+++ dot_config/kickstart/lua/plugins/ai/copilot.lua
@@ -26,6 +26,24 @@ return {
     optional = true,
     dependencies = { 'fang2hou/blink-copilot' },
     opts = {
+      keymap = {
+        preset = 'super-tab',
+        ['<Tab>'] = {
+          function(cmp)
+            if vim.b[vim.api.nvim_get_current_buf()].nes_state then
+              cmp.hide()
+              return (require('copilot-lsp.nes').apply_pending_nes() and require('copilot-lsp.nes').walk_cursor_end_edit())
+            end
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          'snippet_forward',
+          'fallback',
+        },
+      },
       sources = {
         default = { 'copilot' },
         providers = {
@@ -47,10 +65,19 @@ return {
         local nes = require 'copilot-lsp.nes'
 
         vim.g.copilot_nes_debounce = 500
+
+        vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+          callback = function()
+            if not nes.clear() then
+              return
+            end
+          end,
+        })
+
         vim.lsp.enable 'copilot_ls'
 
         -- Keymaps for Copilot Next Edit Suggestions
-        vim.keymap.set('n', '<tab>', function()
+        vim.keymap.set('n', 'g<Tab>', function()
           local _ = nes.walk_cursor_start_edit() or (nes.apply_pending_nes() and nes.walk_cursor_end_edit())
         end, {
           desc = 'Jump to start of suggestion edit or apply pending suggestion & jump to end of edit',
