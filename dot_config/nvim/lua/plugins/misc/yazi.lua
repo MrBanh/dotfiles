3f25fcfd26710fd94fc31d44183187ee8b4e557e
Tony Banh <tbanh@hubspot.com>
2025-06-18 02:00:50 -0700

    remap yazi nvim

--- dot_config/nvim/lua/plugins/misc/yazi.lua
+++ dot_config/nvim/lua/plugins/misc/yazi.lua
@@ -5,20 +5,20 @@ return {
   event = "VeryLazy",
   keys = {
     {
-      "<leader>yf",
+      "-",
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
     {
       -- Open in the current working directory
-      "<leader>yF",
+      "_",
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
     {
       -- NOTE: this requires a version of yazi that includes
       -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-      "<leader>ys",
+      "<leader>fy",
       "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
@@ -27,6 +27,9 @@ return {
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = true,
+    enable_mouse_support = true,
+    floating_window_scaling_factor = 1,
+    yazi_floating_window_border = "none",
     keymaps = {
       show_help = "g?",
       open_file_in_horizontal_split = "<c-s>",
