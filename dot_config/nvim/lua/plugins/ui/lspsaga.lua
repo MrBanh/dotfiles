827345e9d3d573df7c599bf02bc4c636a5a9edd5
Tony Banh <tbanh@hubspot.com>
2025-07-02 23:07:32 -0700

    lspsaga + breadcrumbs

---
+++ dot_config/nvim/lua/plugins/ui/lspsaga.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+    },
+    opts = {
+      lightbulb = {
+        enable = true,
+        virtual_text = false, -- disables just the one at the end of the line
+      },
+      ui = {
+        code_action = "󱐌",
+      },
+      symbol_in_winbar = {
+        enable = false,
+        folder_level = 0,
+        show_file = false,
+      },
+      code_action = {
+        extend_gitsigns = true,
+      },
+    },
+  },
+
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function()
+  --     local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --     -- change a keymap
+  --     keys[#keys + 1] = {
+  --       "<leader>ca",
+  --       ":Lspsaga code_action<CR>",
+  --       desc = "Code Action",
+  --       mode = { "n", "v" },
+  --       has = "codeAction",
+  --     }
+  --   end,
+  -- },
+}
--- dot_config/nvim/lua/plugins/ui/lualine.lua
+++ dot_config/nvim/lua/plugins/ui/lualine.lua
@@ -13,124 +13,116 @@ local disabled_filetypes = {
 }
 
 return {
-  "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    local icons = LazyVim.config.icons
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local icons = LazyVim.config.icons
 
-    -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-    -- local custom_theme = require("lualine.themes.ayu_mirage")
-    local custom_theme = require("lualine.themes.auto")
-    local lualine_modes = { "normal", "inactive" }
-    for _, field in ipairs(lualine_modes) do
-      if custom_theme[field] and custom_theme[field].c then
-        custom_theme[field].c.bg = "NONE"
+      -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+      -- local custom_theme = require("lualine.themes.ayu_mirage")
+      local custom_theme = require("lualine.themes.auto")
+      local lualine_modes = { "normal", "inactive" }
+      for _, field in ipairs(lualine_modes) do
+        if custom_theme[field] and custom_theme[field].c then
+          custom_theme[field].c.bg = "NONE"
+        end
       end
-    end
 
-    opts.options.theme = custom_theme
+      opts.options.theme = custom_theme
 
-    opts.options.disabled_filetypes.winbar = disabled_filetypes
+      opts.options.disabled_filetypes.winbar = disabled_filetypes
 
-    -- separators
-    opts.options.component_separators = ""
-    opts.options.section_separators = { left = "", right = "" }
+      -- separators
+      opts.options.component_separators = ""
+      opts.options.section_separators = { left = "", right = "" }
 
-    -- sections: https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#available-components
-    opts.sections.lualine_a = { { "mode", separator = { left = "", right = "" } } }
-    opts.sections.lualine_c = {
-      LazyVim.lualine.root_dir({
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        cwd = true,
-      }),
-      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-      { LazyVim.lualine.pretty_path({
-        relative = "root",
-        length = 5,
-      }) },
-      {
-        "diagnostics",
-        symbols = {
-          error = icons.diagnostics.Error,
-          warn = icons.diagnostics.Warn,
-          info = icons.diagnostics.Info,
-          hint = icons.diagnostics.Hint,
-        },
-      },
-    }
-    opts.sections.lualine_z = {
-      {
-        function()
-          return " " .. os.date("%R")
-        end,
-        separator = { left = "", right = "" },
-      },
-    }
-
-    -- winbar
-    opts.winbar = {
-      lualine_a = {},
-      lualine_b = {
+      -- sections: https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#available-components
+      opts.sections.lualine_a = { { "mode", separator = { left = "", right = "" } } }
+      opts.sections.lualine_c = {
+        LazyVim.lualine.root_dir({
+          ---@diagnostic disable-next-line: assign-type-mismatch
+          cwd = true,
+        }),
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path({
+          relative = "root",
+          length = 5,
+        }) },
         {
-          "filetype",
-          icon_only = true,
-          padding = { left = 1, right = 0 },
-          color = { fg = "lualine_b_inactive", bg = "NONE" },
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
         },
+      }
+      opts.sections.lualine_z = {
         {
-          "filename",
-          file_status = false,
-          symbols = {},
-          separator = { right = "" },
-          color = { fg = "lualine_b_inactive", bg = "NONE" },
+          function()
+            return " " .. os.date("%R")
+          end,
+          separator = { left = "", right = "" },
         },
-      },
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {},
-    }
+      }
 
-    opts.inactive_winbar = {
-      lualine_a = {},
-      lualine_b = {
-        {
-          "filetype",
-          icon_only = true,
-          padding = { left = 1, right = 0 },
-          color = { fg = Snacks.util.color("Comment"), bg = "NONE" },
+      -- winbar
+      opts.winbar = {
+        lualine_a = {},
+        lualine_b = {
+          {
+            "filetype",
+            icon_only = true,
+            padding = { left = 1, right = 0 },
+            color = { fg = "lualine_b_inactive", bg = "NONE" },
+          },
+          {
+            "filename",
+            file_status = false,
+            symbols = {},
+            separator = { right = "" },
+            color = { fg = "lualine_b_inactive", bg = "NONE" },
+          },
         },
-        {
-          "filename",
-          file_status = false,
-          symbols = {},
-          separator = { right = " " },
-          color = { fg = Snacks.util.color("Comment"), bg = "NONE" },
+        lualine_c = {
+          function()
+            return require("lspsaga.symbol.winbar").get_bar() or ""
+          end,
         },
-      },
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {},
-    }
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      }
 
-    if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-      local trouble = require("trouble")
-      local symbols = trouble.statusline({
-        mode = "symbols",
-        groups = {},
-        title = false,
-        filter = { range = true },
-        format = "{kind_icon}{symbol.name:Normal}",
-        hl_group = "lualine_c_normal",
-      })
-      table.insert(opts.winbar.lualine_c, {
-        symbols and symbols.get,
-        cond = function()
-          return vim.b.trouble_lualine ~= false and symbols.has()
-        end,
-      })
-    end
+      opts.inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {
+          {
+            "filetype",
+            icon_only = true,
+            padding = { left = 1, right = 0 },
+            color = { fg = Snacks.util.color("Comment"), bg = "NONE" },
+          },
+          {
+            "filename",
+            file_status = false,
+            symbols = {},
+            separator = { right = " " },
+            color = { fg = Snacks.util.color("Comment"), bg = "NONE" },
+          },
+        },
+        lualine_c = {
+          function()
+            return require("lspsaga.symbol.winbar").get_bar() or ""
+          end,
+        },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      }
 
-    return opts
-  end,
+      return opts
+    end,
+  },
 }
