6363c5c4dc04684740f527e23a6b9cafea0925e2
Tony Banh <tbanh@hubspot.com>
2025-07-02 23:07:19 -0700

    snacks: add paste + rename for explorer

--- dot_config/nvim/lua/plugins/editor/snacks.lua
+++ dot_config/nvim/lua/plugins/editor/snacks.lua
@@ -38,6 +38,51 @@ return {
           local width = self.list.win:size().width
           self.opts.formatters.file.truncate = width - 6
         end,
+
+        -- yank and paste file + rename if duplicate
+        -- https://github.com/folke/snacks.nvim/discussions/1748
+        explorer_paste_rename = function(picker)
+          local uv = vim.uv or vim.loop
+          local Tree = require("snacks.explorer.tree")
+          local M = require("snacks.explorer.actions")
+
+          local files = vim.split(vim.fn.getreg(vim.v.register or "+") or "", "\n", { plain = true })
+          files = vim.tbl_filter(function(file)
+            return file ~= "" and vim.fn.filereadable(file) == 1
+          end, files)
+
+          if #files == 0 then
+            return Snacks.notify.warn(("The `%s` register does not contain any files"):format(vim.v.register or "+"))
+          end
+          if #files == 1 then
+            local file = files[1]
+            local base = vim.fn.fnamemodify(file, ":t")
+            local dir = picker:dir()
+            Snacks.input({
+              prompt = "Rename pasted file",
+              default = base,
+            }, function(value)
+              if not value or value:find("^%s*$") then
+                return
+              end
+              local target = vim.fs.normalize(dir .. "/" .. value)
+              if uv.fs_stat(target) then
+                Snacks.notify.warn("File already exists:\n- `" .. target .. "`")
+                return
+              end
+              Snacks.picker.util.copy_path(file, target)
+              Tree:refresh(dir)
+              Tree:open(dir)
+              M.update(picker, { target = dir })
+            end)
+          else
+            local dir = picker:dir()
+            Snacks.picker.util.copy(files, dir)
+            Tree:refresh(dir)
+            Tree:open(dir)
+            M.update(picker, { target = dir })
+          end
+        end,
       },
       layout = {
         preset = "ivy",
@@ -107,6 +152,7 @@ return {
             },
             list = {
               keys = {
+                ["p"] = "explorer_paste_rename",
                 ["<c-/>"] = "terminal",
                 ["<LocalLeader>c"] = "tcd",
                 ["<LocalLeader>C"] = { "toggle_cwd", mode = { "n", "i" } },
