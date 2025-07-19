025bff985c56c4937ffe9018f52df3192905a40b
Tony Banh <tbanh@hubspot.com>
2025-07-18 22:55:24 -0700

    Remove vtsls in favor of ts_ls

--- dot_config/nvim/lua/plugins/mason/lsp.lua
+++ dot_config/nvim/lua/plugins/mason/lsp.lua
@@ -3,6 +3,58 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        -- https://github.com/typescript-language-server/typescript-language-server
+        ts_ls = {
+          enabled = true,
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cM",
+              LazyVim.lsp.action["source.addMissingImports.ts"],
+              desc = "Add missing imports",
+            },
+            {
+              "<leader>cu",
+              LazyVim.lsp.action["source.removeUnused.ts"],
+              desc = "Remove unused imports",
+            },
+            {
+              "<leader>cD",
+              LazyVim.lsp.action["source.fixAll.ts"],
+              desc = "Fix all diagnostics",
+            },
+          },
+          cmd = {
+            "typescript-language-server",
+            "--stdio",
+          },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+          init_options = {
+            hostInfo = "neovim",
+          },
+        },
+        vtsls = {
+          enabled = false,
+        },
+      },
+      setup = {
+        vtsls = function()
+          return true
+        end,
+      },
     },
   },
 }
