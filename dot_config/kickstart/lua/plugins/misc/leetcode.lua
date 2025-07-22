a6bb39e028e32a014cf7aa4638cd664d65eb3539
Tony Banh <tbanh@hubspot.com>
2025-07-22 03:39:26 -0700

    update

--- dot_config/kickstart/lua/plugins/misc/leetcode.lua
+++ dot_config/kickstart/lua/plugins/misc/leetcode.lua
@@ -1,15 +1,16 @@
 return {
-  "kawre/leetcode.nvim",
-  build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+  'kawre/leetcode.nvim',
+  cmd = 'Leet',
+  build = ':TSUpdate html', -- if you have `nvim-treesitter` installed
   lazy = true,
   dependencies = {
-    "nvim-telescope/telescope.nvim",
+    'nvim-telescope/telescope.nvim',
     -- "ibhagwan/fzf-lua",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
   },
   opts = {
-    lang = "javascript",
+    lang = 'javascript',
     -- image_support = true,
   },
 }
