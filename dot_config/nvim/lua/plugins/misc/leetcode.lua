2a20a2eabf80f190b6a7325e680a4a23554e6487
Tony Banh <tbanh@hubspot.com>
2025-08-02 20:59:06 -0700

    Add keybindings for leetcode.nvim

--- dot_config/nvim/lua/plugins/misc/leetcode.lua
+++ dot_config/nvim/lua/plugins/misc/leetcode.lua
@@ -16,6 +16,28 @@ return {
     plugins = {
       non_standalone = true,
     },
+    hooks = {
+      ---@type fun()[]
+      ["enter"] = {},
+
+      ---@type fun(question: lc.ui.Question)[]
+      ["question_enter"] = {
+        function(q)
+          local bufnr = q.bufnr
+          vim.b[bufnr].copilot_enabled = false
+          vim.keymap.set("n", "<localleader>d", "<Cmd>Leet desc<CR>", { buffer = bufnr, desc = "LeetCode description" })
+          vim.keymap.set("n", "<localleader>h", "<Cmd>Leet hint<CR>", { buffer = bufnr, desc = "LeetCode hint" })
+          vim.keymap.set("n", "<localleader>i", "<Cmd>Leet info<CR>", { buffer = bufnr, desc = "LeetCode information" })
+          vim.keymap.set("n", "<localleader>l", "<Cmd>Leet list<CR>", { buffer = bufnr, desc = "LeetCode list" })
+          vim.keymap.set("n", "<localleader>r", "<Cmd>Leet run<CR>", { buffer = bufnr, desc = "LeetCode run" })
+          vim.keymap.set("n", "<localleader>s", "<Cmd>Leet submit<CR>", { buffer = bufnr, desc = "LeetCode submit" })
+          vim.keymap.set("n", "<localleader>t", "<Cmd>Leet test<CR>", { buffer = bufnr, desc = "LeetCode test" })
+        end,
+      },
+
+      ---@type fun()[]
+      ["leave"] = {},
+    },
     keys = {
       toggle = { "q" }, ---@type string|string[]
       confirm = { "<CR>" }, ---@type string|string[]
