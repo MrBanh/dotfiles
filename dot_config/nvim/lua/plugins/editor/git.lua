2f5517c1961f8129d48f441af340943d4b590105
Tony Banh <tbanh@hubspot.com>
2025-09-08 22:45:55 -0700

    Remap ]H and [H to go to next/prev hunk including staged

--- dot_config/nvim/lua/plugins/editor/git.lua
+++ dot_config/nvim/lua/plugins/editor/git.lua
@@ -586,9 +586,33 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
       current_line_blame = true,
-      -- if I need to override keys: https://github.com/LazyVim/LazyVim/discussions/4790
     },
     config = function(_, opts)
+      local default_on_attach = opts.on_attach
+
+      opts.on_attach = function(buffer)
+        default_on_attach(buffer)
+
+        -- if I need to override keys: https://github.com/LazyVim/LazyVim/discussions/4790
+        local gs = package.loaded.gitsigns
+
+        vim.keymap.set("n", "]H", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gs.nav_hunk("next", { target = "all" })
+          end
+        end, { desc = "Next Hunk (all)", buffer = buffer })
+
+        vim.keymap.set("n", "[H", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gs.nav_hunk("prev", { target = "all" })
+          end
+        end, { desc = "Prev Hunk (all)", buffer = buffer })
+      end
+
       require("gitsigns").setup(opts)
 
       Snacks.toggle({
