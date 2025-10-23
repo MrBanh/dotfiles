906cfdac473ff8566671a43f5b7427691cd996a9
Tony Banh <tbanh@hubspot.com>
2025-10-22 21:30:02 -0700

    fix(snacks): explorer layout

--- dot_config/nvim/lua/plugins/editor/snacks.lua
+++ dot_config/nvim/lua/plugins/editor/snacks.lua
@@ -134,7 +134,24 @@ return {
         },
         explorer = {
           auto_close = true,
-          layout = { preset = "default", preview = true },
+          layout = {
+            -- preset = "default",
+            preview = true,
+            layout = {
+              box = "horizontal",
+              width = 0.8,
+              min_width = 120,
+              height = 0.8,
+              {
+                box = "vertical",
+                border = true,
+                title = "{title} {live} {flags}",
+                { win = "input", height = 1, border = "bottom" },
+                { win = "list", border = "none" },
+              },
+              { win = "preview", title = "{preview}", border = true, width = 0.5 },
+            },
+          },
           win = {
             input = {
               keys = {
