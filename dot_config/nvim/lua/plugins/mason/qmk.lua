faa6fe3111caacb8e8227f872d06397ab15a5b09
MrBanh <tonybanh@live.com>
2025-11-10 19:06:10 -0800

    qmk.nvim: add layout for corne eyelash keyboard

--- dot_config/nvim/lua/plugins/mason/qmk.lua
+++ dot_config/nvim/lua/plugins/mason/qmk.lua
@@ -1,17 +1,15 @@
 return {
   "codethread/qmk.nvim",
   lazy = true,
-  config = function()
-    local conf = {
-      name = "LAYOUT_preonic_grid",
-      layout = {
-        "_ x x x x x x _ x x x x x x",
-        "_ x x x x x x _ x x x x x x",
-        "_ x x x x x x _ x x x x x x",
-        "_ x x x x x x _ x x x x x x",
-        "_ x x x x x x _ x x x x x x",
-      },
-    }
-    require("qmk").setup(conf)
-  end,
+  cmd = { "QMKFormat" },
+  opts = {
+    name = "LAYOUT_eyelash_corne",
+    variant = "zmk",
+    layout = {
+      "_ x x x x x x _ _ x _ x x x x x x",
+      "_ x x x x x x _ x x x x x x x x x",
+      "_ x x x x x x x _ x _ x x x x x x",
+      "_ _ _ _ x x x _ _ _ _ x x x _ _ _",
+    },
+  },
 }
