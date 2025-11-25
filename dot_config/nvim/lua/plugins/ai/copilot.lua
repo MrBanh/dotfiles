2884881c8ed2ed52ae129b3363240098e010a6c9
Tony Banh <tbanh@hubspot.com>
2025-11-25 02:53:56 -0800

    copilot: cleanup fang2hou/blink-copilot, already provided by LazyExtras

--- dot_config/nvim/lua/plugins/ai/copilot.lua
+++ dot_config/nvim/lua/plugins/ai/copilot.lua
@@ -3,34 +3,12 @@ return {
     "zbirenbaum/copilot.lua",
     opts = {
       suggestion = {
-        enabled = true,
         keymap = {
           dismiss = "<C-c>",
         },
       },
       filetypes = {
         gitcommit = true,
-      },
-    },
-  },
-  {
-    "giuxtaposition/blink-cmp-copilot",
-    enabled = false,
-  },
-  {
-    "saghen/blink.cmp",
-    dependencies = { "fang2hou/blink-copilot" },
-    opts = {
-      sources = {
-        default = { "copilot" },
-        providers = {
-          copilot = {
-            name = "copilot",
-            module = "blink-copilot",
-            score_offset = 100,
-            async = true,
-          },
-        },
       },
     },
   },
