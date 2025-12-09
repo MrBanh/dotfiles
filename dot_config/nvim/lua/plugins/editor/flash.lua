447f4ab3cacc4db4bc3fe5a0b8d1221154b01ab8
Tony Banh <tbanh@hubspot.com>
2025-12-08 22:19:30 -0800

    flash: enable for regular search with / and ?

---
+++ dot_config/nvim/lua/plugins/editor/flash.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      -- options used when flash is activated through
+      -- a regular search with `/` or `?`
+      search = {
+        enabled = true,
+      },
+    },
+  },
+}
