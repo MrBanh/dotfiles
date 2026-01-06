2e132af2b8be2b0abe4905ea62f431c6da61de31
Tony Banh <tbanh@hubspot.com>
2026-01-05 23:25:50 -0800

    nvim: fix flash accidentally jumping to labels, exiting search early

--- dot_config/nvim/lua/plugins/editor/flash.lua
+++ dot_config/nvim/lua/plugins/editor/flash.lua
@@ -1,11 +1,17 @@
+---@module 'flash'
 return {
   "folke/flash.nvim",
+  ---@type Flash.Config
   opts = {
+    ---@type table<string, Flash.Config>
     modes = {
-      -- options used when flash is activated through
-      -- a regular search with `/` or `?`
+      -- options used when flash is activated through a regular search with `/` or `?`
       search = {
         enabled = true,
+        label = {
+          -- otherwise, we might accidentally choose a label that is part of the search pattern and jump to it, causing us to end the search early
+          min_pattern_length = 3, -- minimum pattern length to show labels
+        },
       },
     },
   },
