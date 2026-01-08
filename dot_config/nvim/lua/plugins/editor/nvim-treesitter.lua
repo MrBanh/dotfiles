7e5ebfe9b71b3b77aee08afc93b82d1da0ef1980
Tony Banh <tbanh@hubspot.com>
2026-01-08 01:13:49 -0800

    treesitter: <M-o> and <M-i> for incremental selection

--- dot_config/nvim/lua/plugins/editor/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/editor/nvim-treesitter.lua
@@ -1,11 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {},
-  config = function(_, opts)
-    vim.treesitter.language.register("markdown", "opencode")
-    vim.filetype.add({
-      extension = { lyaml = "yaml", git = "git", keymap = "dts" },
-    })
-    require("nvim-treesitter").setup(opts)
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {},
+    config = function(_, opts)
+      vim.treesitter.language.register("markdown", "opencode")
+      vim.filetype.add({
+        extension = { lyaml = "yaml", git = "git", keymap = "dts" },
+      })
+      require("nvim-treesitter").setup(opts)
+    end,
+  },
+  {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      -- Coming with Nvim 12: https://github.com/neovim/neovim/pull/34011/files
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<M-o>",
+          node_incremental = "<M-o>",
+          scope_incremental = "<M-O>",
+          node_decremental = "<M-i>",
+        },
+      },
+    },
+  },
 }
