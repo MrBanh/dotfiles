a94c01b17611218ab05a9831f430e0dd0d2102e8
Tony Banh <tbanh@hubspot.com>
2026-02-05 01:48:50 -0800

    add: markdowny.nvim for markdown visual keybinds, C-b, C-k, etc

--- dot_config/nvim/lua/plugins/ui/markdown.lua
+++ dot_config/nvim/lua/plugins/ui/markdown.lua
@@ -1,52 +1,61 @@
 local ft = { "md", "markdown", "opencode_output", "codecompanion" }
 
 return {
-  -- Lazy: https://www.lazyvim.org/extras/lang/markdown#render-markdownnvim
-  "MeanderingProgrammer/render-markdown.nvim",
-  lazy = true,
-  ft = ft,
-  opts = {
-    -- Filetypes this plugin will run on.
-    file_types = ft,
-    heading = {
-      icons = { "󰼏 ", "󰎨 ", "󰼑 ", "󰎲 ", "󰼓 ", "󰎴 " },
-      position = "inline",
-      width = "block",
-      left_pad = 1,
-      right_pad = 1,
-    },
-    html = {
-      comment = {
-        conceal = false,
+  -- visual keybinds for markdown, C-b for bold, C-k for link, etc
+  {
+    "antonk52/markdowny.nvim",
+    config = function()
+      require("markdowny").setup()
+    end,
+  },
+  {
+    -- Lazy: https://www.lazyvim.org/extras/lang/markdown#render-markdownnvim
+    "MeanderingProgrammer/render-markdown.nvim",
+    lazy = true,
+    ft = ft,
+    opts = {
+      -- Filetypes this plugin will run on.
+      file_types = ft,
+      heading = {
+        icons = { "󰼏 ", "󰎨 ", "󰼑 ", "󰎲 ", "󰼓 ", "󰎴 " },
+        position = "inline",
+        width = "block",
+        left_pad = 1,
+        right_pad = 1,
       },
-    },
-    code = {
-      language_border = "",
-      language_right = "█",
-      language_left = "█",
-      border = "thin",
-      above = "▄",
-      below = "▀",
-      sign = false,
-    },
-    checkbox = {
-      enabled = true,
-      custom = {
-        todo = { raw = "[~]", rendered = "󰡖 ", highlight = "Orange", scope_highlight = "Orange" },
+      html = {
+        comment = {
+          conceal = false,
+        },
       },
-      checked = {
-        scope_highlight = "@markup.list.checked",
+      code = {
+        language_border = "",
+        language_right = "█",
+        language_left = "█",
+        border = "thin",
+        above = "▄",
+        below = "▀",
+        sign = false,
       },
-      unchecked = {
-        highlight = "Red",
-        scope_highlight = "Red",
+      checkbox = {
+        enabled = true,
+        custom = {
+          todo = { raw = "[~]", rendered = "󰡖 ", highlight = "Orange", scope_highlight = "Orange" },
+        },
+        checked = {
+          scope_highlight = "@markup.list.checked",
+        },
+        unchecked = {
+          highlight = "Red",
+          scope_highlight = "Red",
+        },
       },
-    },
-    bullet = {
-      icons = { "○", "●" },
-    },
-    latex = {
-      enabled = false,
+      bullet = {
+        icons = { "○", "●" },
+      },
+      latex = {
+        enabled = false,
+      },
     },
   },
 }
