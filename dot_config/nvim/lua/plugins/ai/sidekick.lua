a8fb8a7421390ccef7e18839e3b164f1e190b7c2
Tony Banh <tbanh@hubspot.com>
2026-04-21 23:25:13 -0700

    sidekick: add keybindings to <leader>A and <M-?>

--- dot_config/nvim/lua/plugins/ai/sidekick.lua
+++ dot_config/nvim/lua/plugins/ai/sidekick.lua
@@ -42,24 +42,69 @@ return {
       },
     },
   },
-  -- Disable cli keybindings, only keeping nes
   keys = function()
+    local keymap_prefix = "<leader>A"
     return {
+      -- nes
       { "<tab>", LazyVim.cmp.map({ "ai_nes" }, "<tab>"), mode = { "n" }, expr = true },
+
+      -- cli
       {
-        "<c-.>",
-        false,
+        "<M-?>",
+        function()
+          require("sidekick.cli").toggle()
+        end,
+        desc = "Sidekick Toggle",
+        mode = { "n", "t", "i", "x" },
+      },
+      { keymap_prefix, "", desc = "+ai Sidekick cli", mode = { "n", "v" } },
+      {
+        keymap_prefix .. "s",
+        function()
+          require("sidekick.cli").select()
+        end,
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = "Select CLI",
+      },
+      {
+        keymap_prefix .. "d",
+        function()
+          require("sidekick.cli").close()
+        end,
+        desc = "Detach a CLI Session",
+      },
+      {
+        keymap_prefix .. "t",
+        function()
+          require("sidekick.cli").send({ msg = "{this}" })
+        end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        keymap_prefix .. "f",
+        function()
+          require("sidekick.cli").send({ msg = "{file}" })
+        end,
+        desc = "Send File",
+      },
+      {
+        keymap_prefix .. "v",
+        function()
+          require("sidekick.cli").send({ msg = "{selection}" })
+        end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        keymap_prefix .. "p",
+        function()
+          require("sidekick.cli").prompt()
+        end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
       },
     }
   end,
-  -- keys = {
-  --   {
-  --     "<M-/>",
-  --     function()
-  --       require("sidekick.cli").toggle()
-  --     end,
-  --     desc = "Sidekick Toggle",
-  --     mode = { "n", "t", "i", "x" },
-  --   },
-  -- },
 }
