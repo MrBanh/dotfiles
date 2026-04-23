caaad2787c06e7c7ef4884fdb35366aa47f6cc9a
Tony Banh <tbanh@hubspot.com>
2026-04-23 02:19:34 -0700

    fix: refactoring.nvim's print_var and print_loc for js/ts/tsx

--- dot_config/nvim/lua/plugins/editor/refactoring.lua
+++ dot_config/nvim/lua/plugins/editor/refactoring.lua
@@ -1,14 +1,39 @@
-local function js_ts_print_var(opts)
-  local name = opts.identifier_str:gsub("╎", "")
-  return string.format('console.log("[LOGS] %s → ", %s);', name, opts.identifier)
+---@param opts refactor.print_var.code_generation.Opts
+local js_ts_print_var = function(opts)
+  return ([[console.log("[LOGS] %s %s %s → ", %s)]]):format(
+    opts.debug_path:gsub('"', '\\"'),
+    opts.identifier_str:gsub('"', '\\"'),
+    opts.count,
+    opts.identifier
+  )
 end
+
+local print_var_code_generation = {
+  print_var = {
+    javascript = js_ts_print_var,
+    typescript = js_ts_print_var,
+    tsx = js_ts_print_var,
+  },
+}
+
+---@param opts refactor.print_loc.code_generation.Opts
+local js_ts_print_loc = function(opts)
+  return ([[console.log("[LOGS] %s %s -------------")]]):format(opts.debug_path, opts.count)
+end
+
+local print_loc_code_generation = {
+  print_loc = {
+    javascript = js_ts_print_loc,
+    typescript = js_ts_print_loc,
+    tsx = js_ts_print_loc,
+  },
+}
 
 return {
   "ThePrimeagen/refactoring.nvim",
   dependencies = {
     "lewis6991/async.nvim",
   },
-  -- https://github.com/ThePrimeagen/refactoring.nvim/blob/master/lua/refactoring/config.lua
   keys = {
     -- LazyVim's default binding feeds `g@ag`, where `ag` is mini.ai's custom
     -- "around buffer" textobject. It can fail when mini.ai hasn't finished
@@ -31,15 +56,13 @@ return {
     },
   },
   opts = {
+    -- https://github.com/ThePrimeagen/refactoring.nvim/blob/master/lua/refactoring/config.lua
     debug = {
       print_var = {
-        code_generation = {
-          print_var = {
-            javascript = js_ts_print_var,
-            typescript = js_ts_print_var,
-            tsx = js_ts_print_var,
-          },
-        },
+        code_generation = print_var_code_generation,
+      },
+      print_loc = {
+        code_generation = print_loc_code_generation,
       },
     },
   },
