625015809bee02fdc4de0e283fc052d01b723d8c
Tony Banh <tbanh@hubspot.com>
2026-04-25 00:10:24 -0700

    refactoring: revert to older commit and use previous configuration

--- dot_config/nvim/lua/plugins/editor/refactoring.lua
+++ dot_config/nvim/lua/plugins/editor/refactoring.lua
@@ -1,69 +1,159 @@
----@param opts refactor.print_var.code_generation.Opts
-local js_ts_print_var = function(opts)
-  return ([[console.log("[LOGS] %s %s %s → ", %s)]]):format(
-    opts.debug_path:gsub('"', '\\"'),
-    opts.identifier_str:gsub('"', '\\"'),
-    opts.count,
-    opts.identifier
-  )
+local pick = function()
+  local refactoring = require("refactoring")
+  if LazyVim.pick.picker.name == "telescope" then
+    return require("telescope").extensions.refactoring.refactors()
+  elseif LazyVim.pick.picker.name == "fzf" then
+    local fzf_lua = require("fzf-lua")
+    local results = refactoring.get_refactors()
+
+    local opts = {
+      fzf_opts = {},
+      fzf_colors = true,
+      actions = {
+        ["default"] = function(selected)
+          refactoring.refactor(selected[1])
+        end,
+      },
+    }
+    fzf_lua.fzf_exec(results, opts)
+  else
+    refactoring.select_refactor()
+  end
 end
 
-local print_var_code_generation = {
-  print_var = {
-    javascript = js_ts_print_var,
-    typescript = js_ts_print_var,
-    tsx = js_ts_print_var,
-  },
-}
-
----@param opts refactor.print_loc.code_generation.Opts
-local js_ts_print_loc = function(opts)
-  return ([[console.log("[LOGS] %s %s -------------")]]):format(opts.debug_path, opts.count)
-end
-
-local print_loc_code_generation = {
-  print_loc = {
-    javascript = js_ts_print_loc,
-    typescript = js_ts_print_loc,
-    tsx = js_ts_print_loc,
-  },
+local js_ts_print_var_statements = {
+  'console.log("[LOGS] %s → ", %s);',
 }
 
 return {
   "ThePrimeagen/refactoring.nvim",
+  commit = "6784b54587e6d8a6b9ea199318512170ffb9e418",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "lewis6991/async.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
   },
   keys = {
-    -- LazyVim's default binding feeds `g@ag`, where `ag` is mini.ai's custom
-    -- "around buffer" textobject. It can fail when mini.ai hasn't finished
-    -- registering. Here we bypass the operator-pending dance: call cleanup()
-    -- to set operatorfunc + internal state, set the `[` / `]` marks to the
-    -- whole buffer, then invoke the operatorfunc directly.
+    { "<leader>r", "", desc = "+refactor", mode = { "n", "x" } },
+    {
+      "<leader>rs",
+      pick,
+      mode = { "n", "x" },
+      desc = "Refactor",
+    },
+    {
+      "<leader>ri",
+      function()
+        return require("refactoring").refactor("Inline Variable")
+      end,
+      mode = { "n", "x" },
+      desc = "Inline Variable",
+      expr = true,
+    },
+    {
+      "<leader>rb",
+      function()
+        return require("refactoring").refactor("Extract Block")
+      end,
+      mode = { "n", "x" },
+      desc = "Extract Block",
+      expr = true,
+    },
+    {
+      "<leader>rf",
+      function()
+        return require("refactoring").refactor("Extract Block To File")
+      end,
+      mode = { "n", "x" },
+      desc = "Extract Block To File",
+      expr = true,
+    },
+    {
+      "<leader>rP",
+      function()
+        require("refactoring").debug.printf({ below = false })
+      end,
+      desc = "Debug Print",
+    },
+    {
+      "<leader>rp",
+      function()
+        require("refactoring").debug.print_var({ normal = true })
+      end,
+      mode = { "n", "x" },
+      desc = "Debug Print Variable",
+    },
     {
       "<leader>rc",
       function()
-        require("refactoring.debug").cleanup({ restore_view = true })
-        local buf = vim.api.nvim_get_current_buf()
-        local last_line = vim.api.nvim_buf_line_count(buf)
-        local last_line_text = vim.api.nvim_buf_get_lines(buf, last_line - 1, last_line, true)[1] or ""
-        local last_col = math.max(0, #last_line_text - 1)
-        vim.api.nvim_buf_set_mark(buf, "[", 1, 0, {})
-        vim.api.nvim_buf_set_mark(buf, "]", last_line, last_col, {})
-        require("refactoring.debug").debug_operatorfunc("line")
+        require("refactoring").debug.cleanup({})
       end,
-      desc = "Debug Cleanup (whole buffer)",
+      desc = "Debug Cleanup",
+    },
+    {
+      "<leader>rf",
+      function()
+        return require("refactoring").refactor("Extract Function")
+      end,
+      mode = { "n", "x" },
+      desc = "Extract Function",
+      expr = true,
+    },
+    {
+      "<leader>rF",
+      function()
+        return require("refactoring").refactor("Extract Function To File")
+      end,
+      mode = { "n", "x" },
+      desc = "Extract Function To File",
+      expr = true,
+    },
+    {
+      "<leader>rx",
+      function()
+        return require("refactoring").refactor("Extract Variable")
+      end,
+      mode = { "n", "x" },
+      desc = "Extract Variable",
+      expr = true,
+    },
+    {
+      "<leader>rp",
+      function()
+        require("refactoring").debug.print_var({})
+      end,
+      mode = { "n", "x" },
+      desc = "Debug Print Variable",
     },
   },
   opts = {
-    -- https://github.com/ThePrimeagen/refactoring.nvim/blob/master/lua/refactoring/config.lua
-    debug = {
-      print_var = {
-        code_generation = print_var_code_generation,
-      },
-      print_loc = {
-        code_generation = print_loc_code_generation,
-      },
+    prompt_func_return_type = {
+      go = false,
+      java = false,
+      cpp = false,
+      c = false,
+      h = false,
+      hpp = false,
+      cxx = false,
     },
+    prompt_func_param_type = {
+      go = false,
+      java = false,
+      cpp = false,
+      c = false,
+      h = false,
+      hpp = false,
+      cxx = false,
+    },
+    printf_statements = {},
+    print_var_statements = {
+
+      javascript = js_ts_print_var_statements,
+      javascriptreact = js_ts_print_var_statements,
+      typescript = js_ts_print_var_statements,
+      typescriptreact = js_ts_print_var_statements,
+    },
+    show_success_message = true, -- shows a message with information about the refactor on success
+    -- i.e. [Refactor] Inlined 3 variable occurrences
   },
 }
