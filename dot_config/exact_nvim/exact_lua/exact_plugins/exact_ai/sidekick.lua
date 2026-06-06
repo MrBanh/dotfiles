088a0de13307e37b08764e31fe5d1b100ec332b2
Tony Banh <tbanh@hubspot.com>
2026-06-05 23:09:42 -0700

    sidekick.nvim

--- dot_config/exact_nvim/exact_lua/exact_plugins/exact_ai/sidekick.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/exact_ai/sidekick.lua
@@ -33,9 +33,7 @@ return {
         },
       },
       tools = {
-        opencode = {
-          cmd = { "opencode", "--continue" },
-        },
+        opencode = {},
       },
     },
     nes = {
