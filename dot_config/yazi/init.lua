9d467d4a4e96756ed5d2e24ec353393c4f925ca1
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-12 22:31:32 -0700

    yazi: remove save-clipboard-to-file plugin

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,17 +1,3 @@
--- UTILS --
-
--- https://github.com/boydaihungst/save-clipboard-to-file.yazi
-require("save-clipboard-to-file"):setup({
-	-- Position of input file name or path dialog
-	input_position = { "center", w = 70 },
-	-- Position of overwrite confirm dialog
-	overwrite_confirm_position = { "center", w = 70, h = 10 },
-	-- Hide notify
-	hide_notify = false,
-})
-
--- BOOKMARKS / JUMPING --
-
 -- https://github.com/dedukun/bookmarks.yazi
 require("bookmarks"):setup({
 	last_directory = { enable = true, persist = true, mode = "dir" },
