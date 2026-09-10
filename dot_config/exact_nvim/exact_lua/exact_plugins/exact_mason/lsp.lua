4ef735ae9797b3e6bb91c09788fa60b0794af0b5
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-09-09 18:19:54 -0700

    feat(nvim): add workspace-diagnostics.nvim

--- dot_config/exact_nvim/exact_lua/exact_plugins/exact_mason/lsp.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/exact_mason/lsp.lua
@@ -1,26 +1,47 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-    servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            hover = {
-              previewFields = 200, -- default 50; raise to show more struct fields
-              enumsLimit = 100, -- default 5; for large union/enum types
+  {
+    "artemave/workspace-diagnostics.nvim",
+    opts = {},
+    config = function(_, opts)
+      require("workspace-diagnostics").setup(opts)
+
+      Snacks.util.lsp.on(function(buf, client)
+        if client:supports_method("workspace/diagnostic", buf) then
+          vim.lsp.buf.workspace_diagnostics({ client_id = client.id })
+        else
+          require("workspace-diagnostics").populate_workspace_diagnostics(client, buf)
+        end
+      end)
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              hover = {
+                previewFields = 200, -- default 50; raise to show more struct fields
+                enumsLimit = 100, -- default 5; for large union/enum types
+              },
             },
           },
         },
-      },
-      vtsls = {
-        keys = {
-          {
-            "<leader>cu",
-            LazyVim.lsp.action["source.removeUnused.ts"],
-            desc = "Remove unused",
+        vtsls = {
+          keys = {
+            {
+              "<leader>cu",
+              LazyVim.lsp.action["source.removeUnused.ts"],
+              desc = "Remove unused",
+            },
           },
         },
+        taplo = {
+          enabled = false,
+        },
+        tombi = {},
       },
     },
   },
